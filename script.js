865b7ff1a8a42b447c9345b163251b9286b5d5bf
LuisVazquezPerez <luiscarlosvazquezperez@gmail.com>
2025-04-30 02:21:35 -0500

    acomodando modal

--- script.js
+++ script.js
@@ -93,3 +93,25 @@ var Messenger = function(el) {
 console.clear();
 var messenger = new Messenger(document.getElementById('messenger'));
 
+//MODAL
+const now = new Date().getTime();
+const lastShown = localStorage.getItem("welcomeLastShown");
+const oneMinute = 11 * 60 * 1000;
+
+const shouldShowModal =
+  !lastShown || now - parseInt(lastShown) > oneMinute;
+
+if (shouldShowModal) {
+  document.getElementById("welcomeModal").style.display = "flex";
+} else {
+  document.getElementById("welcomeModal").style.display = "none";
+  document.getElementById("mainContent").style.display = "block";
+}
+
+document
+  .getElementById("acceptBtn")
+  .addEventListener("click", function () {
+    localStorage.setItem("welcomeLastShown", new Date().getTime());
+    document.getElementById("welcomeModal").style.display = "none";
+    document.getElementById("mainContent").style.display = "block";
+  });
